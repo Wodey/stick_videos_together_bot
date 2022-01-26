f43ae82f5ebd829de507ebc6b20f2fc7034a33fb
Wodey <54207104+Wodey@users.noreply.github.com>
2022-01-26 15:50:37 +0300

    + VideoMerger class, it can merge several video into one

--- video_merger.py
+++ video_merger.py
@@ -1,6 +1,18 @@
-class Video_merger:
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
+class VideoMerger:
     def __init__(self):
-        pass
+        self.videos = []
+
+    def merge_videos(self, counter):
+        for i in range(counter):
+            self.videos.append(VideoFileClip(f"{i+1}.mp4"))
+        final_video = concatenate_videoclips(self.videos)
+        final_video.write_videofile("final_video.mp4")
+        self.videos = []
+
 
 if __name__ == "__main__":
-    v_g = Video_merger()
+    v_g = VideoMerger()
+    v_g.merge_videos(4)
