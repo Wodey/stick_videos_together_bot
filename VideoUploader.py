3a1f6a1cf6305d48e9e2e1b40e16dd637c5d3fd7
Wodey <54207104+Wodey@users.noreply.github.com>
2022-02-05 01:34:38 +0300

    i make some progress in google api

--- VideoUploader.py
+++ VideoUploader.py
@@ -1,7 +1,11 @@
-import requests
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 URI = "https://www.googleapis.com/upload/youtube/v3/videos"
 
@@ -10,9 +14,9 @@ SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 metadata = {
     "title": "",
     "description": "",
-    "category": "",
+    "category": "10",
     "keywords": "",
-    "privacy": "",
+    "privacy": "private",
 }
 
 FILE_PATH = "final_video.mp4"
@@ -24,20 +28,28 @@ YOUTUBE_API_VERSION = "v3"
 
 
 class VideoUploader:
-    def __init__(self, args):
-        flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-                                       scope=YOUTUBE_UPLOAD_SCOPE)
+    def __init__(self):
+        creds = None
 
-        storage = Storage("%s-oauth2.json")
-        credentials = storage.get()
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-        if credentials is None or credentials.invalid:
-            credentials = run_flow(flow, storage, args)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        try:
+            self.service = build('docs', 'v1', credentials=creds)
 
-
+        except HttpError as err:
+            print(err)
 
     def upload(self):
-        r = requests.post(URI, {})
+        pass
 
 if __name__ == "__main__":
     v_u = VideoUploader()
